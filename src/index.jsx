5cd1efac5c85bb231f19ba8e985febb66b99b1b3
HaixiangYan <haixiang6123@gmail.com>
2021-04-16 22:28:56 +0800

    feat: 调整 example 的结构

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import GrandFather from './GrandFather';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './styles.css'
+
+ReactDOM.render(<GrandFather />, document.getElementById('root'));
